a6922b3198eae7aa090cfd4e5ecbf61c5a7b4981
Abdelzaher-web <68378694+Abdelzaher-web@users.noreply.github.com>
2021-07-26 11:59:24 +0200

    Add all files

---
+++ README.md
@@ -0,0 +1,9 @@
+# Project name : Burger house
+
+# Web developer : Youssef Abdelzaher
+
+# web building date : 25/7/2021
+
+### Design url : https://www.freepik.com/free-psd/american-food-website-app-template_7335301.htm#page=1&query=Web%20templates&position=3
+
+### Download Design direct : https://downloadscdn5.freepik.com/d/7335301/23/2148478/2148477329/american-food-website-app-template.zip?token=exp=1627140427~hmac=d70ff61a5a34387bbfb7c013b6a8219f
---
+++ js/main.js
@@ -0,0 +1,115 @@
+/* 
+Web developer : Youssef Abdelzaher
+Develop date : 25/7/2021 
+*/
+
+$(document).ready(()=>{
+    // Add active class for navbar 
+    $(window).scroll(()=>{
+        if ($(window).scrollTop() > 50) {
+            $(".navbar").addClass("active")
+        }else{
+            $(".navbar").removeClass("active")
+        }
+    })
+
+    // Add event for navbar logo 
+
+    $(".navbar .logo img").click(()=>{
+        $("html,body").animate({
+            scrollTop: 0
+        },100)
+    })
+
+    // Add links-m  and overay and close button
+    $(".navbar .bars").click(()=>{
+        // Add overlay 
+        let overlay = document.createElement("div");
+        overlay.className = "overlay";
+        
+        // Add close button 
+        let close = document.createElement("div");
+        close.className = "close";
+        let icon = document.createElement("i");
+        icon.className = "icon fa fa-times";
+        close.appendChild(icon);
+        overlay.appendChild(close);
+
+        // Add close function 
+        function closeFun() {
+            $(".links-m").removeClass("active");
+            $(close).fadeOut("slow")
+            $(overlay).fadeOut("slow",()=>{
+                overlay.remove();
+            })
+        }
+
+        $(overlay,close).click(()=>{
+            $(".links-m").removeClass("active");
+            closeFun();
+        });
+
+
+        // Add event for links mobile
+        let linksMobile = document.querySelectorAll(".links-m a");
+        linksMobile.forEach((el,index)=>{
+            el.onclick = e =>{
+                if (index === 0) {
+                    e.preventDefault();
+                    closeFun();
+                    $("html,body").animate({
+                        scrollTop: 0
+                    },100)
+                    
+                }
+            }
+        })
+        
+
+        $("body").append(overlay)
+        $(overlay).fadeIn("slow")
+        $(".links-m").addClass("active");
+        
+        $(window).resize(()=>{
+            if (window.innerWidth >= 992 ) {
+                closeFun();
+            }
+        })
+
+        
+    })
+
+    // Add event for two links from links and moble links
+    let links = document.querySelectorAll(".navbar .links a");
+    
+
+    
+    links.forEach((el,index)=>{
+        el.onclick = e =>{
+            if (index === 0) {
+                e.preventDefault();
+                $("html,body").animate({
+                    scrollTop: "0px"
+                },100)
+            }
+        }
+    })
+
+
+    // Add scrollTop button 
+    let scrollTopButton = document.querySelector(".scrollTop");
+    scrollTopButton.onclick = () => {
+        $("html,body").animate({
+            scrollTop: "0px"
+        },100)
+    };
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY <= 100) {
+            $(scrollTopButton).fadeOut("slow");
+        } else {
+            $(scrollTopButton).fadeIn("slow");
+        }
+    });
+
+});
